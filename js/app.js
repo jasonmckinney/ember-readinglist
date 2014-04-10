65f8754baaeeea163f149ccfa0fed0faf51f17e1
Jason McKinney <jason@aslaninteractive.com>
2014-04-10 06:49:40 -0500

    add book route. data model. book display component

--- js/app.js
+++ js/app.js
@@ -2,41 +2,63 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.resource('book', {path: '/books/:book_id'});
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return [
-    	{
-    		id: 1,
-    		title: 'The Lord of the Rings',
-    		author: 'J.R.R. Tolkien',
-    		review: 'While the price of this book is steep, this is easily the best version of this book in print. The gilded pages and high-quality leather look, smell and feel wonderful. This is not the questionable quality leather used on previous versions, this is the real deal. More importantly, this version has, as J.R.R. recorded in letters, reproductions of the Book of Marzubul. These are the pages from the Dwarven book found in the Mines of Moria by Gandalf and the Fellowship. In the begining and ending of the book are also included maps that fold out to render Middle-earth for the reader, again as the author originally wanted.',
-    		rating: 5,
-    		amazon_id: '',
-    		url: 'http://www.amazon.com/Lord-Rings-J-R-R-Tolkien/dp/0544003411',
-    		image: 'images/lotr.jpg'
-    	},
-    	{
-    		id: 2,
-    		title: 'Fight Club',
-    		author: 'Chuck Palahniuk',
-    		review: 'Usually great books are either turned into mediocre films or else great films are made from mediocre books (and we wont even get into the sordid details of the novelizations). Fight Club is one of the rare instances where a great film was made from a great book. It is perhaps unfair to mention the film version while discussing the book as they are actually two very different animals. (And animal is the right word -- perhaps uniquely amongst contemporary novelists, Chuck Palahniuk writes novels that seem to live in the readers hands, often threatening at any minute to lunge for the throat.) While most of the films incidents are in the book and much of the razor-sharp dialouge is reproduced directly from the page, the book actually has a far greater satiric edge than the film. Whereas the film used the story as a celebration of nihilism, the book is far too self-aware to allow itself to truly celebrate anything. As such, it becomes less a call to action and more a devastatingly real portrait of a society that has become so commercialized and codified that even the once primal act of revolution becomes just another submission to pop culture',
-    		rating: 5,
-    		amazon_id: '',
-    		url: 'http://www.amazon.com/Fight-Club-Novel-Chuck-Palahniuk/dp/0393327345',
-    		image: 'images/fightclub.jpg'
-    	},
-    	{
-    		id: 3,
-    		title: 'The Road',
-    		author: 'Cormac McCarthy',
-    		review: '"The Road" is a work of stunning, savage, heartbreaking beauty. Set in the post-apocalyptic hell of an unending nuclear winter, Cormac McCarthy writes about a nameless man and his young son, wandering through a world gone crazy; bleak, cold, dark, where the snow falls down gray; moving south toward the coast, looking somewhere, anywhere, for life and warmth. Nothing grows in this blasted world; people turn into cannibals to survive. We dont know if were looking at the aftermath of a nuclear war, or maybe an extinction level event -- an asteroid or a comet; McCarthy deliberately doesnt tell us, and we come to realize it doesnt matter anyway. Whether man or nature threw a wild pitch, the world is just as dead.',
-    		rating: 5,
-    		amazon_id: '',
-    		url: 'http://www.amazon.com/Road-Cormac-McCarthy/dp/0307387895',
-    		image: 'images/theroad.jpg'
-    	}
-    ];
+    return this.store.findAll('Book');
   }
 });
+
+App.BookRoute = Ember.Route.extend({
+    model: function(params) {
+        return this.store.find('book', params.book_id);
+    }
+});
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend({
+
+
+});
+
+App.Book = DS.Model.extend({
+    title: DS.attr(),
+    author: DS.attr(),
+    review: DS.attr(),
+    rating: DS.attr('number'),
+    amazon_id: DS.attr(),
+    url: function() {
+        return 'http://www.amazon.com/gp/product/'+this.get('amazon_id');
+    }.property('amazon_id'),
+    image: function() {
+        return 'http://images.amazon.com/images/P/'+this.get('amazon_id')+'.01.ZTZZZZ.jpg';
+    }.property('amazon_id')
+});
+
+App.Book.FIXTURES = [
+    {
+        id: 1,
+        title: 'The Lord of the Rings',
+        author: 'J.R.R. Tolkien',
+        review: 'While the price of this book is steep, this is easily the best version of this book in print. The gilded pages and high-quality leather look, smell and feel wonderful. This is not the questionable quality leather used on previous versions, this is the real deal. More importantly, this version has, as J.R.R. recorded in letters, reproductions of the Book of Marzubul. These are the pages from the Dwarven book found in the Mines of Moria by Gandalf and the Fellowship. In the begining and ending of the book are also included maps that fold out to render Middle-earth for the reader, again as the author originally wanted.',
+        rating: 5,
+        amazon_id: '0544003411'
+    },
+    {
+        id: 2,
+        title: 'Fight Club',
+        author: 'Chuck Palahniuk',
+        review: 'Usually great books are either turned into mediocre films or else great films are made from mediocre books (and we wont even get into the sordid details of the novelizations). Fight Club is one of the rare instances where a great film was made from a great book. It is perhaps unfair to mention the film version while discussing the book as they are actually two very different animals. (And animal is the right word -- perhaps uniquely amongst contemporary novelists, Chuck Palahniuk writes novels that seem to live in the readers hands, often threatening at any minute to lunge for the throat.) While most of the films incidents are in the book and much of the razor-sharp dialouge is reproduced directly from the page, the book actually has a far greater satiric edge than the film. Whereas the film used the story as a celebration of nihilism, the book is far too self-aware to allow itself to truly celebrate anything. As such, it becomes less a call to action and more a devastatingly real portrait of a society that has become so commercialized and codified that even the once primal act of revolution becomes just another submission to pop culture',
+        rating: 5,
+        amazon_id: '0393327345'
+    },
+    {
+        id: 3,
+        title: 'The Road',
+        author: 'Cormac McCarthy',
+        review: '"The Road" is a work of stunning, savage, heartbreaking beauty. Set in the post-apocalyptic hell of an unending nuclear winter, Cormac McCarthy writes about a nameless man and his young son, wandering through a world gone crazy; bleak, cold, dark, where the snow falls down gray; moving south toward the coast, looking somewhere, anywhere, for life and warmth. Nothing grows in this blasted world; people turn into cannibals to survive. We dont know if were looking at the aftermath of a nuclear war, or maybe an extinction level event -- an asteroid or a comet; McCarthy deliberately doesnt tell us, and we come to realize it doesnt matter anyway. Whether man or nature threw a wild pitch, the world is just as dead.',
+        rating: 5,
+        amazon_id: '0307387895'
+    }
+];
